fa6575969fcea41338101ee557f89e82fec43f77
shou1027 <longword871@gmail.com>
2024-05-14 22:14:50 +0900

    初期設定＆確認用ファイル追加

---
+++ browser-app/dist/index.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!*********************!*\
+  !*** ./ts/index.ts ***!
+  \*********************/
+
+console.log('hello world');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJtYXBwaW5ncyI6Ijs7Ozs7OztBQUFBLE9BQU8sQ0FBQyxHQUFHLENBQUMsYUFBYSxDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vYnJvd3Nlci1hcHAvLi90cy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnaGVsbG8gd29ybGQnKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
