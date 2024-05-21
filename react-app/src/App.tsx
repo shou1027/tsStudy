0020aa57c6cb6fec671cb7ce4fd4cd776660703a
shou1027 <longword871@gmail.com>
2024-05-22 01:55:33 +0900

    Buttonコンポーネント実装

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,5 +1,6 @@
 import { Text } from "./libs/Text"
 import { Heading } from './libs/Heading';
+import { Button } from './libs/Button';
 
 export const App = () => {
     return (
@@ -9,6 +10,9 @@ export const App = () => {
             <Heading tag="h1">
                 <span>hello, world!</span>
             </Heading>
+            <Button onClick={() => console.log('clicked!')} title="Button" type="primary" width={96} />
+            <Button onClick={() => console.warn('clicked!')} title="Button" type="secondary" />
+            <Button onClick={() => console.error('clicked!')} title="Button" type="error" />
         </>
     )
 }
